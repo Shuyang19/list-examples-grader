99b314e02a6bca2ba630eaa366b38392891029f3
Shuyang19 <shz061@ucsd.edu>
2022-11-28 00:12:44 -0800

    add

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,36 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.ArrayList;
 
 public class TestListExamples {
   // Write your grading tests here!
+  @Test
+  public void testFiler()
+  {
+    StringChecker sc = new StringChecker() 
+    {
+        public boolean checkString(String str)
+        {
+          if(str.contains("a")) 
+          {
+            return true;
+          }
+            return false;
+        }
+    };
+
+      List<String> input = Array.asList("cat", "dog", "apple");
+      List<String> output = Array.asList("cat", "apple");
+      assertEquals(output.toArray(), ListExamples.filter(input, sc).toArray());
+    
+
+  }
+  @Test
+  public void testmerge()
+  {
+    List<String> ls1 = Array.asList("apple", "cat", "pear");
+    List<String> ls2 = Array.asList("banana", "dog");
+    List<String> expected = Array.asList("apple", "banana", "cat", "dog", "pear");
+    assertArrayEquals(expected.toArray(), ListExamples.merge(ls1, ls2));
+  }
 }
--- grade.sh
+++ grade.sh
@@ -1,7 +1,40 @@
 # Create your grading script here
 
-set -e
+
 
 rm -rf student-submission
+rm -rf error.txt
 git clone $1 student-submission
+
+CPATH=".:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar"
+cd student-submission
+if [ -e "ListExamples.java" ] 
+then
+    echo "You submit the correct file, points+1!"
+else
+    echo "Wrong file, please check your submission, no point earned"
+    exit 1
+fi
+
+cd ..
+cp TestListExamples.java student-submission/
+
+cd student-submission
+
+javac -cp $CLASSPATH *.java 2> error.txt
+
+if [ $? -ne 0 ] 
+then
+    echo "You have compile error show below: "
+    cat error.txt
+    echo "You can not get scores from this part"
+    exit 1
+else
+    echo "good job, point+1!"
+fi
+java -cp $CLASSPATH org.junit.runner.JUnitCore TestListExamples > testresult.txt
+cat testresult.txt
+
+
+
 
